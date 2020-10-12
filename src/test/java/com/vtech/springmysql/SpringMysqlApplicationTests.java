881ea32125a48478b730169c9165979a9646f3ce
vinicius dos santos mota <vinicius_dos_santos_mota@hotmail.com>
2020-10-12 00:24:08 -0300

    Start project

---
+++ src/test/java/com/vtech/springmysql/SpringMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtech.springmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
